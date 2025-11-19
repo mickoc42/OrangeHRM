ded212f426d6cfdedf3ce58f25986fa38571dcef
Mickoh <michal.kochanowski@proton.me>
2025-11-19 16:07:39 +0100

    chore: dotenv

---
+++ config/env.config.ts
@@ -0,0 +1,15 @@
+import * as dotenv from "dotenv";
+
+dotenv.config({ override: true });
+
+function requireEnvVariable(envVariableName: string): string {
+  const envVariableValue = process.env[envVariableName];
+  if (envVariableValue === undefined) {
+    throw new Error(`Environment variable ${envVariableName} is not set.`);
+  }
+  return envVariableValue;
+}
+
+export const BASE_URL = requireEnvVariable("BASE_URL");
+export const USERNAME = requireEnvVariable("USERNAME");
+export const USER_PASSWORD = requireEnvVariable("USER_PASSWORD");
